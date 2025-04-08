323b9eafea5fb801185ed16cee16726913579228
Ivan Pavlov <ivanpavlov4@gmail.com>
2025-04-08 21:29:57 +0200

    Implemented removeTask feature

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -64,7 +64,7 @@ class TaskManager {
 
     // 1. Remove a task by name
     public void removeTask(String name) {
-        // TODO: Implement removal logic
+      tasks.removeIf(task -> task.getName().equals(name));
     }
 
     // 2. Find all completed tasks
@@ -120,7 +120,9 @@ public class SI2025Lab1Main {
         manager.addTask("Buy groceries", Priority.LOW, "Personal");
 
         // MISSING: Calls to the new methods that will be implemented
+        manager.removeTask("Submit assignment");
 
         manager.printTasks();
+
     }
 }
